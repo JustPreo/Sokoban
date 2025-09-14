090a2632cb0ae783cf257fa9f61116cc70dbbc8a
JustPreo <impre0hn@gmail.com>
2025-09-14 13:12:52 -0600

    a

--- core/src/main/java/com/sokoban/com/SelectorNiveles/padNiveles.java
+++ core/src/main/java/com/sokoban/com/SelectorNiveles/padNiveles.java
@@ -13,55 +13,45 @@ import com.sokoban.com.Niveles.Lvl1;
 import com.sokoban.com.Juegito;
 import com.sokoban.com.Niveles.Lvl2;
 import com.sokoban.com.Niveles.Lvl3;
-import com.sokoban.com.Tutorialez.Tutorial;
 
 /**
  *
  * @author user
  */
 public class padNiveles {
-    
+
     Texture txt;
     Sprite sprite;
     Rectangle hitbox;
     int nivel;
-    
-    public padNiveles(float x , float y,float TILE,int nivel)
-    {
+
+    public padNiveles(float x, float y, float TILE, int nivel) {
         this.nivel = nivel;
-    switch (nivel) {
-        case 1:
-            txt = new Texture("portales/tp1.png");
-            break;
-        case 2:
-            txt = new Texture("portales/tp2.png");
-            break;
-        case 3:
-            txt = new Texture("portales/tp3.png");
-            break;
-        case 4:
-            txt = new Texture("portales/tp4.png");
-            break;
-        case 5:
-            txt = new Texture("portales/tp5.png");
-            break;
-        case 6:
-            txt = new Texture("portales/tp6.png");
-            break;
-        case 7:
-            txt = new Texture("portales/tp7.png");
-            break;
-        default:
-            txt = new Texture("tp.png"); // por si acaso
-            break;
+        txt = switch (nivel) {
+            case 1 ->
+                new Texture("portales/tp1.png");
+            case 2 ->
+                new Texture("portales/tp2.png");
+            case 3 ->
+                new Texture("portales/tp3.png");
+            case 4 ->
+                new Texture("portales/tp4.png");
+            case 5 ->
+                new Texture("portales/tp5.png");
+            case 6 ->
+                new Texture("portales/tp6.png");
+            case 7 ->
+                new Texture("portales/tp7.png");
+            default ->
+                new Texture("tp.png");
+        }; // por si acaso
+        sprite = new Sprite(txt);
+        sprite.setSize(TILE, TILE);
+        sprite.setPosition(x, y);
+        hitbox = new Rectangle(x, y, sprite.getWidth(), sprite.getHeight());
+
     }
-    sprite = new Sprite(txt);
-    sprite.setSize(TILE, TILE);
-    sprite.setPosition(x, y);
-    hitbox = new Rectangle(x, y, sprite.getWidth(), sprite.getHeight());
-    
-    }
-    
+
     public void render(SpriteBatch batch) {
         sprite.draw(batch);
     }
@@ -69,44 +59,41 @@ public class padNiveles {
     public Rectangle getHitbox() {
         return hitbox;
     }
+
     public void dispose() {
-        if (txt != null) txt.dispose();
-    }
-    public void irANivel()
-    {
-        switch(nivel)
-        {
-            case 1:
-                System.out.println("Nivel 1");
-                ((Juegito) Gdx.app.getApplicationListener()).setScreen(new Lvl1());
-                break;
-            case 2:
-                System.out.println("Nivel 2");
-                ((Juegito) Gdx.app.getApplicationListener()).setScreen(new Lvl2());
-                break;
-            case 3:
-                System.out.println("Nivel 3");
-                ((Juegito) Gdx.app.getApplicationListener()).setScreen(new Lvl3());
-                break;
-            case 4:
-                System.out.println("Nivel 4");
-                break;
-            case 5:
-                System.out.println("Nivel 5");
-                break;
-            case 6:
-                System.out.println("Nivel 6");
-                break;
-            case 7:
-                System.out.println("Nivel 7");
-                break;
-        
+        if (txt != null) {
+            txt.dispose();
         }
     }
-    
-    public int getNivel()
-    {
-    return nivel;
+
+    public void irANivel() {
+        switch (nivel) {
+            case 1 -> {
+                System.out.println("Nivel 1");
+                ((Juegito) Gdx.app.getApplicationListener()).setScreen(new Lvl1());
+            }
+            case 2 -> {
+                System.out.println("Nivel 2");
+                ((Juegito) Gdx.app.getApplicationListener()).setScreen(new Lvl2());
+            }
+            case 3 -> {
+                System.out.println("Nivel 3");
+                ((Juegito) Gdx.app.getApplicationListener()).setScreen(new Lvl3());
+            }
+            case 4 ->
+                System.out.println("Nivel 4");
+            case 5 ->
+                System.out.println("Nivel 5");
+            case 6 ->
+                System.out.println("Nivel 6");
+            case 7 ->
+                System.out.println("Nivel 7");
+
+        }
     }
-    
+
+    public int getNivel() {
+        return nivel;
+    }
+
 }
